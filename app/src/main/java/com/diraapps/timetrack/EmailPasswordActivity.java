1c34484556d051b98e8dc37c2bc93818d309d607
rperez1991 <macsebos@hotmail.com>
2019-06-02 16:34:09 +0200

    Notificaciones

--- app/src/main/java/com/diraapps/timetrack/EmailPasswordActivity.java
+++ app/src/main/java/com/diraapps/timetrack/EmailPasswordActivity.java
@@ -16,7 +16,10 @@
 
 package com.diraapps.timetrack;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -37,6 +40,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
 
 import java.util.Date;
 import java.util.Iterator;
@@ -66,6 +70,7 @@ public class EmailPasswordActivity extends BaseActivity implements
         mEmailField = findViewById(R.id.fieldEmail);
         mPasswordField = findViewById(R.id.fieldPassword);
 
+
         // Buttons
         findViewById(R.id.emailSignInButton).setOnClickListener(this);
         findViewById(R.id.signOutButton).setOnClickListener(this);
@@ -75,11 +80,39 @@ public class EmailPasswordActivity extends BaseActivity implements
         mAuth = FirebaseAuth.getInstance();
         // [END initialize_auth]
 
+        updateDB(mAuth.getCurrentUser());
+        updateUI(mAuth.getCurrentUser());
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            String channelId  = "123";
+            String channelName = "123";
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(new NotificationChannel(channelId,
+                    channelName, NotificationManager.IMPORTANCE_LOW));
+        }
+
+        // If a notification message is tapped, any data accompanying the notification
+        // message is available in the intent extras. In this sample the launcher
+        // intent is fired when the notification is tapped, so any accompanying data would
+        // be handled here. If you want a different intent fired, set the click_action
+        // field of the notification message to the desired intent. The launcher intent
+        // is used when no click_action is specified.
+        //
+        // Handle possible data accompanying notification message.
+        // [START handle_data_extras]
+        if (getIntent().getExtras() != null) {
+            for (String key : getIntent().getExtras().keySet()) {
+                Object value = getIntent().getExtras().get(key);
+                Log.d(TAG, "Key: " + key + " Value: " + value);
+            }
+        }
+
         findViewById(R.id.enter).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (dbTracking != null && dbTracking != null) {
-
                     try {
                         if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
                             ActivityCompat.requestPermissions(EmailPasswordActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
@@ -99,9 +132,13 @@ public class EmailPasswordActivity extends BaseActivity implements
                     ultimoTiempo.child("enter").setValue(new Date());
                     ultimoTiempo.child("coordenadas_enter").child("latitud").setValue(latitude);
                     ultimoTiempo.child("coordenadas_enter").child("longitud").setValue(longitude);
+
                     findViewById(R.id.enter).setEnabled(true);
                     findViewById(R.id.exit).setEnabled(false);
                 }
+                updateDB(mAuth.getCurrentUser());
+                updateUI(mAuth.getCurrentUser());
+
             }
         });
         findViewById(R.id.exit).setOnClickListener(new View.OnClickListener() {
@@ -126,6 +163,9 @@ public class EmailPasswordActivity extends BaseActivity implements
                     findViewById(R.id.enter).setEnabled(false);
                     findViewById(R.id.exit).setEnabled(true);
                 }
+                updateDB(mAuth.getCurrentUser());
+                updateUI(mAuth.getCurrentUser());
+
             }
         });
     }
